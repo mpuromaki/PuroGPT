8a61d29b56596a7c9dc637d10915bdcc96d5f13e
Marko Puromäki <23620092+mpuromaki@users.noreply.github.com>
2023-02-11 22:28:01 +0200

    Initial short term memory functionality

--- purogpt.py
+++ purogpt.py
@@ -1,9 +1,11 @@
 import os
 from pathlib import Path
+from datetime import datetime
 import discord
 import openai
 
-CHATBOT_NAME = "PuroGPT"
+DEBUG = True
+LOGFILE = Path(f'logs/{datetime.now().strftime("%Y%m%d_%H%M%S")}.log')
 
 DISCORD_API_KEY = Path('.discord_api_key').read_text()
 DISCORD_CHANNEL_IDS = Path('discord_channel_ids.list').read_text().splitlines()
@@ -11,6 +13,59 @@ DISCORD_RESPOND_IDS = Path('discord_respond_ids.list').read_text().splitlines()
 
 OPENAI_API_KEY = Path('.openai_api_key').read_text()
 OPENAI_MODEL = "text-davinci-003"
+
+# --- MEMORY FUNCTIONS ---
+
+MEMORY_SHORT_TERM = list()
+def add_to_memory(text):
+    if DEBUG:
+        print(f"[d] Add to memory: {text}")
+    MEMORY_SHORT_TERM.append(text)
+    # Length of short term memory must be limited
+    if len(MEMORY_SHORT_TERM) > 15:
+        if DEBUG:
+            print(f"[d] MEMORY_SHORT_TERM: {MEMORY_SHORT_TERM}")
+            print(f"[d] Removing from memory: {MEMORY_SHORT_TERM[0]}")
+        MEMORY_SHORT_TERM.pop(0)
+
+
+# --- TEXT FUNCTIONS ---
+
+def append_text(path, text, encoding=None, errors=None):
+   with path.open("a", encoding=encoding, errors=errors) as f:
+       f.write(text)
+
+def load_kv(filepath):
+    static_conf = dict()
+    for line in filepath.read_text().splitlines():
+        print(line)
+        kv = line.split('=', 1)
+        print(kv)
+        static_conf[kv[0]] = kv[1]
+    return static_conf
+
+STATIC_CONF = load_kv(Path('static.kv'))
+
+def replace_all(source_text):
+    source_text = replace_static(source_text, STATIC_CONF)
+    source_text = replace_dynamic(source_text)
+    source_text = replace_blocks(source_text)
+    return source_text
+
+def replace_static(text, static_kv):
+    for k, v in static_kv.items():
+        text = text.replace(k, v)
+    return text
+
+def replace_dynamic(text):
+    now = datetime.now()
+    text = text.replace('<DYN-DATETIME>', now.strftime("%A, %Y-%m-%d %H:%M:%S"))
+    return text
+
+def replace_blocks(text):
+    # Recent messages from short term memory
+    text = text.replace('<BLOCK-RECENT-MESSAGES>', '\n'.join(MEMORY_SHORT_TERM))
+    return text
 
 # --- OPENAI ---
 
@@ -36,7 +91,19 @@ discord_intents.message_content = True
 
 discord_client = discord.Client(intents=discord_intents)
 
-
+def parse_discord_message(message):
+    now = datetime.now()
+    retdict = dict()
+    retdict["datetime"] = now.strftime("%Y-%m-%d %H:%M:%S")
+    retdict["name"] = message.author.name.strip()
+    retdict["content"] = message.content.strip()
+    
+    # Replace Discord user id for our bot with proper name
+    for _id in DISCORD_RESPOND_IDS:
+        retdict["content"] = retdict["content"].replace(_id, STATIC_CONF['<VAR-CHATBOT-NAME>'])
+            
+    return retdict
+    
 @discord_client.event
 async def on_ready():
     print(f'{discord_client.user} has connected to Discord!')
@@ -51,22 +118,41 @@ async def on_message(message):
     if message.author == discord_client.user:
         return
 
-    # Skip messages not meant to me
-    if not any(_id in message.content for _id in DISCORD_RESPOND_IDS):
+    #if DEBUG:
+    #    print(f"[d] Received message: {message}")
+
+    if any(_id in message.content for _id in DISCORD_RESPOND_IDS):
+        # Record to memory and continue
+        msg = parse_discord_message(message)
+        add_to_memory(f"[{msg['datetime']}] {msg['name']}: {msg['content']}")
+    else:
+        # Record to memory and skip
+        msg = parse_discord_message(message)
+        add_to_memory(f"[{msg['datetime']}] {msg['name']}: {msg['content']}")
         return
 
     # Start the response generation
     async with message.channel.typing():
-        msg_from = message.author.name
-        msg_content = message.content
-        for _id in DISCORD_RESPOND_IDS:
-            msg_content = msg_content.replace(_id, CHATBOT_NAME)
-        msg_content = msg_content.strip()
-        print(f">> {msg_from}: {msg_content}")
+        # Search long term memory for related messages
+
+        # Get facts from external sources
+
+        if DEBUG:
+            print(f"[d] >> {msg}")
+
+        # Create the prompt
+        prompt = replace_all(Path('templates/chat.template').read_text())
+
+        if DEBUG:
+            append_text(LOGFILE, '\n' + prompt + '\n')
 
         # Send prompt to openai
-        openai_response = ask_openai(msg_content)
+        openai_response = ask_openai(prompt)
         print(f"<< {repr(openai_response)}")
+
+        if DEBUG:
+            append_text(LOGFILE, '\n' + repr(openai_response) + '\n')
+
 
     # Send response to discord
     await message.channel.send(openai_response["response"])
@@ -78,11 +164,13 @@ if __name__ == "__main__":
     print("=== Starting PuroGPT ===")
     print(f"[i] Discord API-KEY: {DISCORD_API_KEY}")
     print(f"[i] OpenAI API-KEY: {OPENAI_API_KEY}")
+    print(f"[i] Static configuration: {repr(STATIC_CONF)}")
 
     # Setup OpenAI
     openai.api_key = OPENAI_API_KEY
 
     # Setup Discord
     discord_client.run(DISCORD_API_KEY)
+
     
     print("=== SHUTDOWN ===")
